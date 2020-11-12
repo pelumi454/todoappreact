7b616909eff7aa37852969870b78ed7d4db0a022
pelumi454 <amoochristianah454@gmail.com>
2020-11-12 14:23:37 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Todos from './Components/Todos'
+import AddForm from './Components/Addform'
+
+ class App extends Component {
+  state = {
+    todos: [
+    {id: 1, content: 'buy some milk'},
+    {id: 2, content: 'withdraw money at the bank'}
+    ]
+} 
+ deleteTodo = (id) => {
+   const todos = this.state.todos.filter(todo => {
+      return todo.id !== id 
+    });
+    this.setState({
+      todos
+    })
+ }
+ addTodo = (todo) => {
+  todo.id = Math.random()
+  let todos = [...this.state.todos, todo];
+  this.setState({
+    todos
+  })
+ }
+  render() {
+    return (
+      <div className="todo-app container">
+         <h1 className="center blue-text">Todo's</h1>
+         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+         <AddForm addTodo ={this.addTodo}></AddForm>
+      </div>
+    );
+  }
+}
+
+export default App
+
+
